94852099a6e39cab524d8b91c0856ae1715230a1
ThiagoDe <17tawi@gmail.com>
2022-10-15 00:48:03 -0300

    Frontend with svg maps

--- frontend/src/components/Public.jsx
+++ frontend/src/components/Public.jsx
@@ -2,22 +2,70 @@ import { Link } from 'react-router-dom'
 import SvgUs from './Us'
 
 const Public = () => {
+    window.addEventListener('DOMContentLoaded', (event) => {
+    const usMap = document.getElementsByTagName('path')
+    
+    const addMouseover = function (e) {
+                    e.stopPropagation()
+                    if (e.target.tagName === 'path') {
+                        var content = e.target.dataset.name;
+                        var content_id = e.target.dataset.id;
+                        
+                        boxInfo.style.display = "block";
+                        boxInfo.innerHTML = content + " " + content_id;
+                        
+                        boxInfo.style.opacity = "100%";
+                    }
+                 
+                }
+
+    const addMouseout =  function(e){
+                    e.stopPropagation()
+                    boxInfo.style.opacity = "0%"
+                }
+    
+    let gameModeBtn = document.getElementById("checkbox");
+    var boxInfo = document.getElementById('details-box');
+    gameModeBtn.addEventListener('click', function(e) {
+        e.stopPropagation();
+        console.log('event game mode',e)
+            
+
+        if (e.target.checked) {
+            console.log(gameModeBtn.checked, 'target checked')
+            for (let i = 0; i < usMap.length; i++) {
+                usMap[i].addEventListener('mouseover', addMouseover);
+
+                usMap[i].addEventListener('mouseout',addMouseout)
+
+                window.onmousemove = function (e) {
+                    var x = e.clientX;
+                    var y = e.clientY;
+                    boxInfo.style.top = (y + 25) + 'px';
+                    boxInfo.style.left = (x) + 'px';
+                };
+            }
+        } else if (!e.target.checked) {
+            console.log(gameModeBtn.checked)
+            for (let i = 0; i < usMap.length; i++) {
+
+                usMap[i].removeEventListener('mouseover', addMouseover)
+                usMap[i].removeEventListener('mouseout', addMouseout)
+                }
+            }
+        })
+
+    })
+
     const content = (
         <section className="public">
             <header>
                 <h1>Welcome to <span className="nowrap">Geo-Quiz!</span></h1>
             </header>
             <main className="public__main">
-                {/* <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-                <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
-                <br />
-                <p>Owner: Dan Davidson</p> */}
+                
                 <SvgUs />
+                
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
