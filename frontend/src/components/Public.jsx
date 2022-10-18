80df13c7a34a70e64bd7124d4d295cadd40f9d7c
ThiagoDe <17tawi@gmail.com>
2022-10-17 19:11:00 -0700

    Front window event ok

--- frontend/src/components/Public.jsx
+++ frontend/src/components/Public.jsx
@@ -1,61 +1,76 @@
 import { Link } from 'react-router-dom'
 import SvgUs from './Us'
+import React, { useState, useEffect } from 'react'
 
 const Public = () => {
-    window.addEventListener('DOMContentLoaded', (event) => {
-    const usMap = document.getElementsByTagName('path')
-    
-    const addMouseover = function (e) {
-                    e.stopPropagation()
+    // const [gameModeBtn, setGameModeBtn] = useState(false)
+    const [usMap, setUsMap] = useState(null)
+    const [stateName, setStateName ] = useState('')
+    const [stateId, setStateId] = useState('')
+    const [boxInfo, setBoxInfo] = useState('none')
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+    const [uniqState, setUniqState] = useState(null)
+
+    const addMouseover = React.useCallback((e) => {
+
                     if (e.target.tagName === 'path') {
-                        var content = e.target.dataset.name;
-                        var content_id = e.target.dataset.id;
                         
-                        boxInfo.style.display = "block";
-                        boxInfo.innerHTML = content + " " + content_id;
-                        
-                        boxInfo.style.opacity = "100%";
+                        setStateName(e.target.dataset.name)
+                        setStateId(e.target.dataset.id)
                     }
                  
-                }
-
-    const addMouseout =  function(e){
-                    e.stopPropagation()
-                    boxInfo.style.opacity = "0%"
-                }
+                })
     
-    let gameModeBtn = document.getElementById("checkbox");
-    var boxInfo = document.getElementById('details-box');
-    gameModeBtn.addEventListener('click', function(e) {
-        e.stopPropagation();
-        console.log('event game mode',e)
-            
-
-        if (e.target.checked) {
-            console.log(gameModeBtn.checked, 'target checked')
-            for (let i = 0; i < usMap.length; i++) {
-                usMap[i].addEventListener('mouseover', addMouseover);
-
-                usMap[i].addEventListener('mouseout',addMouseout)
-
-                window.onmousemove = function (e) {
-                    var x = e.clientX;
-                    var y = e.clientY;
-                    boxInfo.style.top = (y + 25) + 'px';
-                    boxInfo.style.left = (x) + 'px';
-                };
-            }
-        } else if (!e.target.checked) {
-            console.log(gameModeBtn.checked)
-            for (let i = 0; i < usMap.length; i++) {
-
-                usMap[i].removeEventListener('mouseover', addMouseover)
-                usMap[i].removeEventListener('mouseout', addMouseout)
-                }
-            }
-        })
-
+    const addMouseout = React.useCallback(() => {
+        // e.stopPropagation()
+        console.log('mouseout')
+        boxInfo.style.opacity = "0%"
     })
+
+    const windowMouse = () => {
+        window.onmousemove = function (e) {  
+                setUniqState(e.target)
+                setX(e.clientX)
+                setY(e.clientY)  
+                boxInfo.style.display = "block";
+                boxInfo.innerHTML = stateName + " " + stateId;
+                boxInfo.style.opacity = "100%";            
+                boxInfo.style.top = (y + 25) + 'px';
+                boxInfo.style.left = (x) + 'px';
+            };
+    }
+
+    useEffect (() => {
+        setUsMap(document.getElementsByTagName('path'))
+        setBoxInfo(document.getElementById('details-box'))
+
+        
+        if (uniqState) {
+            console.log(uniqState, 'test in')
+            uniqState.addEventListener('mouseover', addMouseover)
+            uniqState.addEventListener('mouseout',addMouseout)
+
+        }
+        window.onmousemove = function (e) {  
+                setUniqState(e.target)
+                setX(e.clientX)
+                setY(e.clientY)  
+                boxInfo.style.display = "block";
+                boxInfo.innerHTML = stateName + " " + stateId;
+                boxInfo.style.opacity = "100%";            
+                boxInfo.style.top = (y + 25) + 'px';
+                boxInfo.style.left = (x) + 'px';
+            };
+        
+    }, [x, y, usMap, stateName, stateId, boxInfo, uniqState, addMouseout, addMouseover] );
+    
+    
+ 
+
+    
+    
+
 
     const content = (
         <section className="public">
@@ -63,8 +78,11 @@ const Public = () => {
                 <h1>Welcome to <span className="nowrap">Geo-Quiz!</span></h1>
             </header>
             <main className="public__main">
-                
-                <SvgUs />
+
+                <div id="details-box"></div>
+                <div onMouseEnter={addMouseover} onMouseLeave={addMouseout}>
+                    <SvgUs />
+                </div>
                 
             </main>
             <footer>
