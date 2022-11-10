e6ed76c848a669caa4351b57995b86eb7ad26f19
ThiagoDe <17tawi@gmail.com>
2022-11-10 10:24:03 -0800

    First deployment

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -13,7 +13,7 @@ export const store = configureStore({
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
+    devTools: false
 })
 
 setupListeners(store.dispatch)
