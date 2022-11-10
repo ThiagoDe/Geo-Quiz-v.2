5b5363bcf5c33a4e3706535b82fbd947330c11b8
ThiagoDe <17tawi@gmail.com>
2022-11-10 15:12:54 -0800

    Logout fixed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://geoquiz-v2.onrender.com',
+    'https://geoquiz-v2.onrender.com/'
 ]
 
 module.exports = allowedOrigins
--- frontend/src/features/auth/PersistLogin.js
+++ frontend/src/features/auth/PersistLogin.js
@@ -20,7 +20,7 @@ const PersistLogin = () => {
         isLoading,
         isSuccess,
         isError,
-        error
+        // error
     }] = useRefreshMutation()
 
 
