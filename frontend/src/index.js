f033ab7dab8d1021b98a2924f66a65aca5a16014
ThiagoDe <17tawi@gmail.com>
2022-11-10 11:09:35 -0800

    merge main

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { store } from './app/store'
 import { Provider } from 'react-redux';
-import { desableReactDevTools} from '@fvilers/disable-react-devtools'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV === 'production') desableReactDevTools()
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
